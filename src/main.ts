be1a48e2de31402e94d8fa0e1bad1b86eb1a9add
cuishiyi6 <1960393658@qq.com>
2021-07-07 15:27:01 +0800

    i

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,19 @@ import { AppModule } from './AppModule';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Resultintercepotor } from './common/interceptor/Resultintercepotor';
 import { AllExceptionFilter } from './common/filter/ExceptionFilter';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const config = new DocumentBuilder()
     .setTitle('itemall项目接口文档')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.enableCors();
+  app.useStaticAssets('upload');
   app.useGlobalInterceptors(new Resultintercepotor());
-
   app.useGlobalFilters(new AllExceptionFilter());
   await app.listen(3000);
 }
